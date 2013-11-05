ca1189ee1ab84c1af785ee5b1ed0c25bd7db3b2b
Mark Litwintschik <mark@marksblogg.com>
2013-11-05 12:30:59 +0200

    Create virtualenv.sh

---
+++ django/virtualenv.sh
@@ -0,0 +1,16 @@
+export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python
+export WORKON_HOME=$HOME/.virtualenvs
+source /usr/local/bin/virtualenvwrapper.sh
+export PIP_VIRTUALENV_BASE=$HOME/.virtualenvs
+export PIP_RESPECT_VIRTUALENV=true
+export PYTHONPATH=[location-of-django-applications]
+alias v=workon
+alias v.deactivate=deactivate
+alias v.mk='mkvirtualenv --no-site-packages'
+alias v.mk_withsitepackages='mkvirtualenv'
+alias v.rm=rmvirtualenv
+alias v.switch=workon
+alias v.add2virtualenv=add2virtualenv
+alias v.cdsitepackages=cdsitepackages
+alias v.cd=cdvirtualenv
+alias v.lssitepackages=lssitepackages
