89c173f030ce6ed7c081f969c842ff9d472cb1a3
Mark Litwintschik <mark@marksblogg.com>
2013-11-05 12:07:12 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,2 @@
-source bash/config.sh bash/prompt.sh bash/aliases.sh
+source bash/config.sh bash/prompt.sh bash/aliases.sh \
+  django/completion.sh functions/extract.sh functions/fab.sh git/prompt.sh git/completion.sh
