0d554008111d14157d1b19fc2cdc890c3199ed17
Mark Litwintschik <mark@marksblogg.com>
2013-11-20 16:48:58 +0200

    Update aliases.sh
    Added a few more aliases for git and fab.

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -17,15 +17,19 @@ alias l='ls -CF'
 
 alias npm='/usr/local/bin/npm' && alias nodejs='/usr/local/bin/node'
 
-
+alias c=clear
 alias h=history
 
+alias ga='git add -A'
+alias gb='git branch'
+alias gc='git commit'
 alias gd='git df'
-alias gs='git status'
+alias gl='git lg'
 alias gp='git push origin master'
+alias gs='git status'
 
 alias fd='fab deploy'
+alias ft='fab test'
 
 alias rs='python manage.py runserver 0.0.0.0:8000'
 alias rpyc='find . | grep pyc | xargs rm'
-alias c=clear
